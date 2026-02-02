8d9f26fe719f8862a808de7c0db70ef9fdbfca00
Caps Lock <46106832+capsey@users.noreply.github.com>
2026-02-02 20:08:01 +0100

    let there be light

---
+++ src/client/java/net/capsey/nestweaver/NestweaverOriginClient.java
@@ -0,0 +1,10 @@
+package net.capsey.nestweaver;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NestweaverOriginClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
